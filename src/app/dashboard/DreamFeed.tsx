486b96a549da624ac27eb2101c1f2930995a5efa
Jason Korol <ejkorol@gmail.com>
2024-07-01 17:33:56 -0400

    sorting dreams and dynamically rendering date

--- src/app/dashboard/DreamFeed.tsx
+++ src/app/dashboard/DreamFeed.tsx
@@ -1,20 +1,28 @@
 import { Dream } from "@/types/dashboard";
 import { ScrollShadow } from "@nextui-org/react";
 import DreamCard from "@/app/dashboard/DreamCard";
+import { format, isSameDay } from "date-fns";
 
 interface DreamFeedProps {
   dreams: Dream[];
 };
 
-export default function DreamFeed({ dreams }: DreamFeedProps) {
+export default async function DreamFeed({ dreams }: DreamFeedProps) {
+
+  const sortedDreams = [...dreams].sort((a, b) => {
+    return new Date(a.created_at).getTime() - new Date(b.created_at).getTime();
+  });
+
   return (
     <ScrollShadow hideScrollBar size={100} className="flex flex-col mt-12 w-full pl-6 pr-6">
-      <div className="flex w-full mb-4">
-        <h2 className="text-2xl tracking-wide font-light">Sun Mar 24</h2>
-      </div>
-      {dreams.map((dream: Dream) => {
-        return <DreamCard key={dream.id} dream={dream} />
-      })}
+      {sortedDreams.map((dream, index) => (
+        <div key={dream.id} className="">
+          {(index === 0 || !isSameDay(new Date(sortedDreams[index - 1].created_at), new Date(dream.created_at))) && (
+            <h2 className="text-2xl tracking-wide mb-4">{format(new Date(dream.created_at), "iii MMM dd")}</h2>
+          )}
+          <DreamCard key={dream.id} dream={dream} />
+        </div>
+      ))}
     </ScrollShadow>
   );
 };
