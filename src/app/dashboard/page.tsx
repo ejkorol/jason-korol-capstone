abfad17bb7feccad839913f202812f8a15a1311b
Jason Korol <ejkorol@gmail.com>
2024-07-01 17:07:32 -0400

    fetch dreams, add types

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,26 @@
+import { Dream } from "@/types/dashboard";
+
 import StatusBar from "@/app/dashboard/StatusBar";
 import Search from "@/app/dashboard/Search";
-import DreamFeed from "@/app/dashboard/Feed";
+import DreamFeed from "@/app/dashboard/DreamFeed";
+
+async function fetchDreams(): Promise<Dream[]> {
+  const API_URL = process.env.API_URL;
+  const res = await fetch(`${API_URL}/users/dreams/1`, { cache: 'no-cache' });
+  if (!res.ok) {
+    throw new Error('Failed to fetch');
+  };
+
+  return res.json();
+};
 
 export default async function Dashboard() {
+  const dreams = await fetchDreams();
   return (
     <section className="flex flex-col h-screen w-full">
       <StatusBar />
       <Search />
-      <DreamFeed />
+      <DreamFeed dreams={dreams} />
     </section>
   );
 };
