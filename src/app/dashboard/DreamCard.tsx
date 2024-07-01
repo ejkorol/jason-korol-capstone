086280f1304680a11696e000df235d04b953d264
Jason Korol <ejkorol@gmail.com>
2024-07-01 17:08:33 -0400

    catch props, render dreamcard component dynamically

--- src/app/dashboard/DreamCard.tsx
+++ src/app/dashboard/DreamCard.tsx
@@ -1,22 +1,31 @@
+import { Dream, Tag } from "@/types/dashboard";
 import { Chip } from "@nextui-org/react";
 import { Spacer } from "@nextui-org/react";
 import { Card, CardBody } from "@nextui-org/react";
 
-export default function DreamCard() {
+interface DreamCardProps {
+  dream: Dream;
+};
+
+export default async function DreamCard({dream: {dream_title, dream_context, tags}}: DreamCardProps) {
   return (
     <div className="h-full w-full mb-6">
       <Card radius="sm" shadow="none" style={{ backgroundColor: "#F4F4F5" }}>
         <CardBody>
           <div className="flex flex-row justify-between items-center">
-            <div><h2 className="text-xl tracking-wide">Fleeting Memories</h2></div>
+            <div><h2 className="text-xl tracking-wide">{dream_title}</h2></div>
             <div><p className="text-sm font-mono tracking-tight">4:21 AM</p></div>
           </div>
           <Spacer y={2}/>
           <div>
-            <p className="text-sm tracking-wide font-light" style={{ color: "#616161" }}>  I find myself wandering through a vast, mist-covered forest illuminated by the soft glow of moonlight. As I traverse the winding pathways I am for some reason...</p>
+            <p className="text-sm tracking-wide font-light" style={{ color: "#616161" }}>{dream_context}</p>
           </div>
           <Spacer y={2}/>
-          <Chip className="text-sm" variant="shadow" radius="full" size="md" style={{ backgroundColor: "#212121", color: "#ffffff" }}>Nostalgic Journey</Chip>
+          <div className="flex gap-2">
+          {tags.map((tag: Tag) => {
+            return <Chip key={tag.id} className="text-sm" variant="shadow" radius="full" size="md" style={{ backgroundColor: "#212121", color: "#ffffff" }}>{tag.tag_name}</Chip>
+          })}
+          </div>
         </CardBody>
       </Card>
     </div>
