2290c11d59671588e33cfdc8143d8e0606cb4faf
Jason Korol <ejkorol@gmail.com>
2024-07-01 02:04:19 -0400

    add uuid to server dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['knex', 'bcrypt', 'jsonwebtoken'],
+    serverComponentsExternalPackages: ['knex', 'bcrypt', 'jsonwebtoken', 'uuid'],
   },
 };
 
