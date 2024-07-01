f5265d3c4a5c7f3cf12c92906981ea246fbde4c9
Jason Korol <ejkorol@gmail.com>
2024-07-01 17:08:02 -0400

    prop drilling dreams from page, adding types

--- src/app/dashboard/DreamFeed.tsx
+++ src/app/dashboard/DreamFeed.tsx
@@ -1,18 +1,20 @@
+import { Dream } from "@/types/dashboard";
 import { ScrollShadow } from "@nextui-org/react";
 import DreamCard from "@/app/dashboard/DreamCard";
 
-export default function DreamFeed() {
+interface DreamFeedProps {
+  dreams: Dream[];
+};
+
+export default function DreamFeed({ dreams }: DreamFeedProps) {
   return (
     <ScrollShadow hideScrollBar size={100} className="flex flex-col mt-12 w-full pl-6 pr-6">
       <div className="flex w-full mb-4">
         <h2 className="text-2xl tracking-wide font-light">Sun Mar 24</h2>
       </div>
-      <DreamCard />
-      <DreamCard />
-      <DreamCard />
-      <DreamCard />
-      <DreamCard />
-      <DreamCard />
+      {dreams.map((dream: Dream) => {
+        return <DreamCard key={dream.id} dream={dream} />
+      })}
     </ScrollShadow>
   );
 };
