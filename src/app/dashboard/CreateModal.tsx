25d59bcebe7b9a300537c2ee684fb40503f13071
Jason Korol <ejkorol@gmail.com>
2024-07-01 21:39:21 -0400

    add initial object stream functionality

--- src/app/dashboard/CreateModal.tsx
+++ src/app/dashboard/CreateModal.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 import {
   Button,
   Spacer,
   Input,
+  Chip,
   Textarea,
   Modal,
   ModalContent,
@@ -12,25 +13,57 @@ import {
 } from "@nextui-org/react";
 import ArrowLeft from "@/app/icons/ArrowLeftIcon";
 import ArrowRight from "@/app/icons/ArrowRightIcon";
+import { experimental_useObject as useObject } from "ai/react";
+import { useState } from "react";
+import { dreamSchema } from "@/app/api/dreams/dreamSchema";
+import useGreeting from "@/utils/hooks/useGreeting";
 
 export default function CreateModal(props: object) {
+
+  const [ dream, setDream ] = useState({
+    context: "",
+    userId: 1
+  });
+  const { date } = useGreeting();
+  const { object, submit } = useObject({
+    api: '/api/dreams',
+    schema: dreamSchema
+  });
+
   return (
     <Modal hideCloseButton backdrop="blur" radius="md" shadow="sm" isOpen={props.isOpen} onOpenChange={props.onOpenChange}>
       <ModalContent>
         <ModalHeader>
           <div className="mt-4 flex flex-col">
-            <h1 className="text-3xl tracking-wide font-light">How did you sleep?</h1>
+            <h1 className="text-3xl tracking-wide font-light">
+              {!object?.analysis && ("Sleep sunked?")}
+              {object?.analysis && (`${object.analysis.dream_title}`)}
+            </h1>
             <Spacer y={2}/>
-            <h2 className="text-md tracaking-wide font-light">Wednesday, April 3<span className="">rd</span></h2>
+            <h2 className="text-md tracaking-wide font-light">{date}</h2>
           </div>
         </ModalHeader>
         <ModalBody>
           <div className="mt-8 w-full">
-            <Textarea minRows={20} size="lg" fullWidth label="What did you dream about?" placeholder="Write down as much as you can remember, including your emotion and how the dream made you feel."/>
-            <Spacer y={4}/>
-            <Input fullWidth size="lg" label="Do you remember any symbols?"/>
-            <Spacer y={4}/>
-            <Input fullWidth size="lg" label="How was your quality of sleep?"/>
+            {!object?.analysis && (
+              <Textarea minRows={20} value={dream.context} onChange={(e) => { setDream({
+                context: e.target.value,
+                userId: 1
+              }) }} size="lg" fullWidth label="What did you dream about?" />
+            )}
+            {object?.analysis && (
+              <>
+                <Textarea minRows={20} readOnly value={object.analysis.dream_analysis} size="lg" fullWidth label="What did you dream about?" />
+                <Spacer y={4} />
+                <h3>The symbols in your dream</h3>
+                {object.analysis.dream_symbols?.split(",").forEach((symbol) => {
+                  <Chip className="text-sm" variant="shadow" radius="full" size="md">{symbol}</Chip>
+                })}
+                <Spacer y={4} />
+                <h3>What you described</h3>
+                <p className="text-sm text-neutral-500">{dream.context}</p>
+              </>
+            )}
           </div>
         </ModalBody>
         <ModalFooter>
@@ -39,7 +72,7 @@ export default function CreateModal(props: object) {
               <Button isIconOnly variant="light" size="lg"><ArrowLeft color="#bdbdbd" size={18} stroke={1.5}/></Button>
             </div>
             <div className="flex">
-              <Button variant="light" size="lg" endContent={ <ArrowRight color="#212121" size={18} stroke={1.5}/> }>Add to Journal</Button>
+              <Button variant="light" size="lg" onClick={() => { submit(dream) }} endContent={ <ArrowRight color="#212121" size={18} stroke={1.5}/> }>Add to Journal</Button>
             </div>
           </div>
         </ModalFooter>
