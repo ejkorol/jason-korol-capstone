3de34228205208d1b403f08404321cef9713f569
Jason Korol <ejkorol@gmail.com>
2024-07-01 21:43:14 -0400

    add z index on dock to prevent overflow

--- src/app/components/Dock/Dock.tsx
+++ src/app/components/Dock/Dock.tsx
@@ -7,7 +7,7 @@ import { Button, Link } from "@nextui-org/react";
 export default async function Dock() {
 
   return (
-    <div className="w-full fixed bottom-0">
+    <div className="w-full fixed bottom-0 z-10">
       <div className="flex relative flex-col bg-gray-100/70 backdrop-blur-md">
         <div className="pb-4 pt-4 pl-4 pr-4 flex justify-between items-center">
           <div><Button as={Link} href="/dashboard" isIconOnly variant="light" size="lg" radius="full"><HomeIcon color="#212121" /></Button></div>
