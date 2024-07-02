7045f959a918eb55ad2c0f13b812365ada549e2a
Jason Korol <ejkorol@gmail.com>
2024-07-01 22:56:19 -0400

    update dock style

--- src/app/components/Dock/Dock.tsx
+++ src/app/components/Dock/Dock.tsx
@@ -7,8 +7,8 @@ import { Button, Link } from "@nextui-org/react";
 export default async function Dock() {
 
   return (
-    <div className="w-full fixed bottom-0 z-10">
-      <div className="flex relative flex-col bg-gray-100/70 backdrop-blur-md">
+    <div className="w-full fixed bottom-0">
+      <div className="flex relative w-[90%] m-auto mb-4 rounded-lg flex-col backdrop-blur-lg bg-neutral-200/90 z-10">
         <div className="pb-4 pt-4 pl-4 pr-4 flex justify-between items-center">
           <div><Button as={Link} href="/dashboard" isIconOnly variant="light" size="lg" radius="full"><HomeIcon color="#212121" /></Button></div>
           <div><Button as={Link} href="/dashboard/symbols" isIconOnly variant="light" size="lg" radius="full"><JournalIcon color="#212121"/></Button></div>
