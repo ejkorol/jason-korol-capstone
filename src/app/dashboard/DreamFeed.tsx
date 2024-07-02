07645616e96e862821580fc2d62847fa61c89e56
Jason Korol <ejkorol@gmail.com>
2024-07-01 23:42:40 -0400

    add conditional check if dreams is not empty

--- src/app/dashboard/DreamFeed.tsx
+++ src/app/dashboard/DreamFeed.tsx
@@ -2,6 +2,7 @@ import { Dream } from "@/types/dashboard";
 import { ScrollShadow } from "@nextui-org/react";
 import DreamCard from "@/app/dashboard/DreamCard";
 import { format, isSameDay } from "date-fns";
+import MoonLogo from "@/app/icons/MoonLogo";
 
 interface DreamFeedProps {
   dreams: Dream[];
@@ -12,6 +13,15 @@ export default async function DreamFeed({ dreams }: DreamFeedProps) {
   const sortedDreams = [...dreams].sort((a, b) => {
     return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
   });
+
+  if (dreams.length <= 0) {
+    return (
+      <div className="h-[60%] flex flex-col items-center justify-center">
+        <MoonLogo size={80} color="#D9D9D9" />
+        <h2 className="mt-6 text-lg text-neutral-300">Sleep not yet sunked...</h2>
+      </div>
+    );
+  };
 
   return (
     <ScrollShadow hideScrollBar size={100} className="flex flex-col mt-12 w-full pl-6 pr-6">
