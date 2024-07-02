228c0c1bc44fbb2755dc63f80be83a70c26960d2
Jason Korol <ejkorol@gmail.com>
2024-07-02 02:55:11 -0400

    fetch symbol data upon load

--- src/app/dashboard/symbols/SymbolModal.tsx
+++ src/app/dashboard/symbols/SymbolModal.tsx
@@ -11,20 +11,46 @@ import {
   Image
 } from "@nextui-org/react";
 import ArrowLeft from "@/app/icons/ArrowLeftIcon";
+import { useState, useEffect } from "react";
+import { Symbol } from "@/types/dashboard";
 
-export default function SymbolModal(props: any) {
+interface SymbolModalProps {
+  isOpen: Boolean;
+  onOpenChange: Function;
+  symbolId: Number;
+}
+
+export default function SymbolModal({isOpen, onOpenChange, symbolId}: SymbolModalProps) {
+
+  const [ symbol, setSymbol ] = useState<Symbol>();
+
+  useEffect(() => {
+    async function fetchSymbol() {
+      try {
+        const res = await fetch(`/api/users/1/symbols/${symbolId}`);
+        const data = await res.json();
+        setSymbol(data[0]);
+      } catch (e) {
+        console.error(e);
+      };
+    };
+    fetchSymbol();
+  }, [symbolId]);
+
+  if (!symbol) return <p>loading...</p>
+
   return (
-    <Modal hideCloseButton backdrop="blur" radius="md" shadow="sm" isOpen={props.isOpen} onOpenChange={props.onOpenChange}>
+    <Modal hideCloseButton backdrop="blur" radius="md" shadow="sm" isOpen={isOpen} onOpenChange={onOpenChange}>
       <ModalContent>
         <ModalHeader>
           <div className="mt-4 flex flex-col">
             <Image isBlurred width={60} className="aspect-square object-cover" radius="sm" src="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"/>
-            <h1 className="mt-4 text-3xl tracking-wide font-light">Book</h1>
+            <h1 className="mt-4 text-3xl tracking-wide font-light">{symbol.symbol_name}</h1>
           </div>
         </ModalHeader>
         <ModalBody>
           <div className="w-full">
-            <Textarea readOnly minRows={20} size="lg" fullWidth label="In your context" placeholder="This is where the information about this symbol will appear in the context of the user"/>
+            <Textarea readOnly minRows={20} size="lg" fullWidth label="In your context" placeholder={symbol.symbol_analysis} />
           </div>
           <div className="mt-4 w-full">
             <h2 className="mb-4">Appears in:</h2>
