065b56b5d58ddc3e6181381e0bff26965b1967d8
Jason Korol <ejkorol@gmail.com>
2024-07-01 20:16:09 -0400

    update symbol types

--- src/types/dashboard/Symbol.ts
+++ src/types/dashboard/Symbol.ts
@@ -1,4 +1,9 @@
 export interface Symbol {
   id: number;
+  dream_id: number;
+  user_id: number;
   symbol_name: string;
+  symbol_analysis: string;
+  created_at: string;
+  updated_at: string;
 };
