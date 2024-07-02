ee4d1e7ae88890d5f6c5fdb292bea657c56f7374
Jason Korol <ejkorol@gmail.com>
2024-07-01 22:58:27 -0400

    update dock height

--- src/app/components/Dock/Dock.tsx
+++ src/app/components/Dock/Dock.tsx
@@ -8,12 +8,12 @@ export default async function Dock() {
 
   return (
     <div className="w-full fixed bottom-0">
-      <div className="flex relative w-[90%] m-auto mb-4 rounded-lg flex-col backdrop-blur-lg bg-neutral-200/90 z-10">
+      <div className="flex relative w-[90%] m-auto mb-4 h-[4rem] rounded-lg flex-col backdrop-blur-lg bg-neutral-200/90 z-10">
         <div className="pb-4 pt-4 pl-4 pr-4 flex justify-between items-center">
-          <div><Button as={Link} href="/dashboard" isIconOnly variant="light" size="lg" radius="full"><HomeIcon color="#212121" /></Button></div>
-          <div><Button as={Link} href="/dashboard/symbols" isIconOnly variant="light" size="lg" radius="full"><JournalIcon color="#212121"/></Button></div>
-          <div><Button isIconOnly variant="light" size="lg" radius="full"><QuestionIcon color="#212121" /></Button></div>
-          <div><Button isIconOnly variant="light" size="lg" radius="full"><FriendsIcon color="#212121" /></Button></div>
+          <div><Button as={Link} href="/dashboard" isIconOnly variant="light" size="sm" radius="full"><HomeIcon color="#212121" /></Button></div>
+          <div><Button as={Link} href="/dashboard/symbols" isIconOnly variant="light" size="sm" radius="full"><JournalIcon color="#212121"/></Button></div>
+          <div><Button isIconOnly variant="light" size="sm" radius="full"><QuestionIcon color="#212121" /></Button></div>
+          <div><Button isIconOnly variant="light" size="sm" radius="full"><FriendsIcon color="#212121" /></Button></div>
         </div>
       </div>
     </div>
