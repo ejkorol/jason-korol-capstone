dffc885f1fe7224315da1404e9ac1ed4924faf46
Jason Korol <ejkorol@gmail.com>
2024-07-03 04:12:49 -0400

    refactor image generation functions, abstracting to pass in context and prompt

--- src/app/api/dreams/imagePrompt.ts
+++ src/app/api/dreams/imagePrompt.ts
@@ -3,3 +3,9 @@ export function imagePrompt(context: string) {
     `Create an image of a surreal dreamscape where this is the description: ${context}. The scene should have a slightly surreal atmosphere, with elements that blend reality and fantasy. Use muted, soft colors to give the image a dreamy, ethereal quality. Ensure the overall composition feels otherworldly and slightly disjointed, evoking the mysterious nature of dreams.`
   )
 };
+
+export function symbolPrompt(context: string) {
+  return (
+    `Create a surreal image of this symbol that appears in my dreams: ${context}.`
+  )
+};
