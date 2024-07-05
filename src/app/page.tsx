7c004f25a1962c6e3028c1462058927993429e29
Jason Korol <ejkorol@gmail.com>
2024-07-04 21:25:56 -0400

    check if user is logged in to display dashboard button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { cookies } from "next/headers";
 import MoonLogo from "@/app/icons/MoonLogo";
 import StarryBackground from "@/app/components/StarryBackground/StarryBackground";
 
@@ -7,6 +8,8 @@ import {
 } from "@nextui-org/react";
 
 export default async function Home() {
+
+  const isLoggedIn = cookies().get("Authorization");
 
   return (
     <section className="flex w-screen h-screen items-center justify-center">
@@ -18,8 +21,15 @@ export default async function Home() {
           <h2 className="text-2xl mt-4 tracking-wide font-light">Welcome to Lucid</h2>
           <p className="font-extralight text-md">Lucid helps you gain clarity and understanding of your dreams, empowering you to explore your subconscious mind and uncover hidden insights in your waking day.</p>
           <div className="flex gap-4 mt-8">
-            <Button href="/signup" as={Link} variant="shadow" size="lg" radius="full" className="text-sm" style={{ backgroundColor: "#212121", color: "#ffffff" }}>Let's Get Started</Button>
-            <Button href="/login" as={Link} size="lg" radius="full" className="text-sm" variant="light" style={{ color: "#212121" }}>Login Instead</Button>
+            {isLoggedIn 
+              ? <>
+                  <Button href="/dashboard" as={Link} variant="shadow" size="lg" radius="full" className="text-sm" style={{ backgroundColor: "#212121", color: "#ffffff" }}>Go to dashboard</Button>
+                </>
+              : <>
+                  <Button href="/signup" as={Link} variant="shadow" size="lg" radius="full" className="text-sm" style={{ backgroundColor: "#212121", color: "#ffffff" }}>Let's Get Started</Button>
+                  <Button href="/login" as={Link} size="lg" radius="full" className="text-sm" variant="light" style={{ color: "#212121" }}>Login Instead</Button>
+                </>
+            }
           </div>
         </main>
       </div>
