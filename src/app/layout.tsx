60c4e620ef71cbd7048a89ad33a40c0cdc921bcf
Jason Korol <ejkorol@gmail.com>
2024-07-04 23:49:23 -0400

    add metadata for manifest in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,25 @@ import "./globals.css";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "lucid",
+  title: "Lucid",
   description: "(im)proved dream journal",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["lucid", "dream journal", "ai", "dreams"],
+  authors: [
+    { name: "Jason Korol" },
+    {
+      name: "Jason Korol",
+      url: "https://www.linkedin.com/in/ejkorol"
+    },
+  ],
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
-export default function RootLayout({ children, }: Readonly<{children: React.ReactNode;}>) {
+export default function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body className={inter.className}>
