a54bfb6a1c30797b83bffe6e6455eff4f9541be6
Jason Korol <ejkorol@gmail.com>
2024-07-04 23:49:10 -0400

    export combined config for next with pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
-/** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
+
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     serverComponentsExternalPackages: ['knex', 'bcrypt', 'jsonwebtoken', 'uuid'],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
+
+export default withPWA(pwaConfig)(nextConfig);
