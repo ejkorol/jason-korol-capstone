48334f8af7187071ac7eb810dacab216a5e89e9d
Jason Korol <ejkorol@gmail.com>
2024-07-06 16:17:01 -0400

    add next theme as provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
           <Toaster position="top-right"/>
