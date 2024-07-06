9cdaa7a9d63a1e1c5c85465b36c1c93d0d4b530d
Jason Korol <ejkorol@gmail.com>
2024-07-06 01:51:38 -0400

    add data for zodiacs

---
+++ src/lib/data/zodiacSigns.ts
@@ -0,0 +1,16 @@
+const zodiacSigns: ZodiacSign[] = [
+  { name: "Aries", symbol: "♈" },
+  { name: "Taurus", symbol: "♉" },
+  { name: "Gemini", symbol: "♊" },
+  { name: "Cancer", symbol: "♋" },
+  { name: "Leo", symbol: "♌" },
+  { name: "Virgo", symbol: "♍" },
+  { name: "Libra", symbol: "♎" },
+  { name: "Scorpio", symbol: "♏" },
+  { name: "Sagittarius", symbol: "♐" },
+  { name: "Capricorn", symbol: "♑" },
+  { name: "Aquarius", symbol: "♒" },
+  { name: "Pisces", symbol: "♓" }
+];
+
+export default zodiacSigns;
