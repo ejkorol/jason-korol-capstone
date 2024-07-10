ab3bd5bb6eecc892af104ab365918e87b5678ca9
Jason Korol <ejkorol@gmail.com>
2024-07-09 22:47:18 -0400

    add where builder to match multiple fields

--- src/app/api/users/[userId]/search/[query]/route.ts
+++ src/app/api/users/[userId]/search/[query]/route.ts
@@ -3,7 +3,10 @@ import db from "@/lib/db-instance";
 export async function GET(_req: Request, { params }: { params: { query: string, userId: number } }) {
   try {
     const foundUsers = await db("users")
-      .where("users.username", "like", `%${params.query}%`)
+      .where(builder => {
+        builder.where("users.username", "like", `%${params.query}%`)
+          .orWhere("users.first_name", "like", `%${params.query}%`);
+      })
       .andWhere("users.id", "<>", params.userId)
       .select(
         "users.id",
@@ -15,7 +18,7 @@ export async function GET(_req: Request, { params }: { params: { query: string, 
     if (foundUsers.length > 0) {
       return new Response(JSON.stringify(foundUsers), { status: 200, headers: { "Content-Type": "application/json" } });
     } else {
-      return;
+      return new Response(JSON.stringify([]), { status: 200, headers: { "Content-Type": "application/json" } });
     }
   } catch (e: any) {
     return new Response(JSON.stringify({ error: e.message }), { status: 500, headers: { "Content-Type": "application/json" } });
