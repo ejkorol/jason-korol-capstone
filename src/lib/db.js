ecb831532835ee7aaa86648da82fa532bd548632
Jason Korol <ejkorol@gmail.com>
2024-07-10 01:04:48 -0400

    update port variable

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,6 +3,7 @@ const config = {
   connection: {
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     charset: "utf8",
